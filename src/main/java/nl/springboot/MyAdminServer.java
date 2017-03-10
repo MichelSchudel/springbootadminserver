069651e1a7cc70e3d6c71c4264fe74dc7abe8ba9
Schudel, MJ (Michel) <m.j.schudel@rn.rabobank.nl>
2017-03-10 20:24:42 +0100

    Initial commit

---
+++ src/main/java/nl/springboot/MyAdminServer.java
@@ -0,0 +1,16 @@
+package nl.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class MyAdminServer {
+
+    public static void main(String args[]) {
+        SpringApplication.run(MyAdminServer.class);
+    }
+
+}
